0d37544921ff2d05b1e8f5b6d44c51a7d6c490e1
inaho <envelopeof-inaho170@yahoo.co.jp>
2021-09-08 13:00:34 +0900

    初コミット

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-app id="app">
+    <div id="menu">
+      <router-link to="/">Home</router-link>
+      <router-link to="/Calendar">Calendar</router-link>
+    </div>
+      <router-view/>
+  </v-app>
+</template>
---
+++ src/views/Calendar.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- ここにコンポーネントの HTML を記述 -->
+  <div class="calendar">
+  <!-- 自分で作成したコンポーネントのタグは使用しないとエラーになる（eslintに引っかかる） -->
+  <calendar></calendar>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Calendar from "@/components/Calendar.vue";
+
+// ここに JavaScript で Vueオブジェクトを定義 
+export default {
+  name: "calendar",
+  components: {
+    Calendar    // 表示させたい自作コンポーネントを記述（今回はcomponentsフォルダーのCalendarコンポーネント）
+  }
+};
+</script>
